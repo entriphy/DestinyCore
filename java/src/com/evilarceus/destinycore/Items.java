7105456ca4e55a8bc210c3aadc966c2e3c466bc0
evilarceus <evilarceus@gmail.com>
2016-11-05 18:00:05 -0800

    Completed restructuring

--- java/src/com/evilarceus/destinycore/Items.java
+++ java/src/com/evilarceus/destinycore/Items.java
@@ -1,27 +1,25 @@
 package com.evilarceus.destinycore;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.google.gson.stream.JsonReader;
 
 import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
 /**
  * Created by matthewfinerty on 10/15/16.
  */
 public class Items {
 
 
-
-    public static void main(String[] args) throws IOException {
-        //Test code here
-        infoBox("The item is " + hashToWeapon("2882093969"), "Result");
-    }
+    static String apiKey = "<API Key here>";
 
 
     //Use this dialog box to test... stuff
@@ -37,7 +35,6 @@ public class Items {
         URL obj = new URL(url);
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
         con.setRequestMethod("GET");
-        //con.setRequestProperty("X-API-KEY", apiKey);
 
         BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
         String inputLine;
@@ -56,6 +53,38 @@ public class Items {
         JsonElement result = json.getAsJsonObject("Response").getAsJsonObject("data").getAsJsonObject("inventoryItem").get("itemName");
 
         return result.toString().replaceAll("\"", "");
+
+    }
+	//Currently does not work
+    public static String getItemIcon(String hash)
+    {
+        try {
+            String url = "https://www.bungie.net/Platform/Destiny/Manifest/InventoryItem/" + hash;
+
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("X-API-KEY", apiKey);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            String response = "";
+
+            while ((inputLine = in.readLine()) != null) {
+                response += inputLine;
+            }
+
+            in.close();
+
+            JsonParser parser = new JsonParser();
+            JsonObject json = parser.parse(response).getAsJsonObject();
+            String icon = json.getAsJsonObject("Response").getAsJsonObject("data").get("inventoryItem").getAsString();
+            return icon;
+        } catch(Exception e) {
+            e.printStackTrace();
+            return "";
+        }
+
 
     }
 }
